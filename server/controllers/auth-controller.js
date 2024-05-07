efe95947b733492b5fae1b2914f5afd7a0c30dc8
Deep-20 <49397575+Deep-20@users.noreply.github.com>
2024-05-07 15:44:54 -0400

    Securing User Passwords using bcrypt

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,31 +1,38 @@
 const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
 
 const home = async (req, res) => {
-    try{
-        res.status(200).json({message: "Welcome to root by router!!"});
+  try {
+    res.status(200).json({ message: "Welcome to root by router!!" });
+  } catch (error) {
+    res.status(400).send({ msg: "Page Not Found!!" });
+  }
+};
+
+const register = async (req, res) => {
+  try {
+    const { username, email, phone, password } = req.body;
+
+    const UserExist = await User.findOne({ email: email });
+
+    if (UserExist) {
+      return res.status(400).json({ msg: "User Already Exists!!" });
     }
-    catch(error){
-        res.status(400).send({msg : "Page Not Found!!"});
-    }
-}
 
-const register = async(req, res) => {
-    try{
-        const {username, email, phone, password} = req.body;
+    // const saltRound = 10;
+    // const hash_password = await bcrypt.hash(password, saltRound);
 
-        const UserExist = await User.findOne({email: email});
+    const UserCreated = await User.create({
+      username,
+      email,
+      phone,
+      password,
+    });
 
-        if(UserExist){
-            return res.status(400).json({msg: "User Already Exists!!"});
-        }
-        
-        const UserCreated = await User.create({username, email, phone, password});
+    res.status(201).json({ message: UserCreated });
+  } catch (error) {
+    res.status(400).send({ msg: "Page Not Found!!" });
+  }
+};
 
-        res.status(200).json({message: UserCreated});
-    }
-    catch(error){
-        res.status(400).send({msg : "Page Not Found!!"});
-    }
-}
-
-module.exports = {home, register};
+module.exports = { home, register };
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     username : {
@@ -22,6 +23,23 @@ const userSchema = new mongoose.Schema({
         default: false
     }
 });
+
+userSchema.pre("save", async function(next){
+    const user = this;
+
+    if(!user.isModified("password")){
+        next();
+    }
+
+    try {
+        const saltRound = await bcrypt.genSalt(10);
+        const hash_password = await bcrypt.hash(user.password, saltRound);
+        user.password = hash_password;
+
+    } catch (error) {
+        next(error);
+    }
+})
 
 const User = new mongoose.model("User", userSchema);
 
