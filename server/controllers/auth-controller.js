e1febdbb8d50d9a62a34bc7593a00ce64672797e
Deep-20 <49397575+Deep-20@users.noreply.github.com>
2024-05-27 23:19:31 -0400

    Added JWT Authentication and login functionality

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -29,10 +29,42 @@ const register = async (req, res) => {
       password,
     });
 
-    res.status(201).json({ message: UserCreated });
+    res.status(201).json({ 
+      message: "Registration Successful", 
+      token: await UserCreated.generateToken(), 
+      userId: UserCreated._id.toString()
+    });
   } catch (error) {
-    res.status(400).send({ msg: "Page Not Found!!" });
+    res.status(500).json("Internal Server Error!!");
   }
 };
 
-module.exports = { home, register };
+const login = async(req, res) => {
+  try {
+    const {email, password} = req.body;
+
+    const userExist = await User.findOne({email});
+    
+    if(!userExist){
+      return res.status(400).json({message: "Invalid Credential!!"});
+    }
+
+    const user = await userExist.comparePassword(password);
+
+    if(user){
+      res.status(200).json({ 
+        message: "Login Successful", 
+        token: await userExist.generateToken(), 
+        userId: userExist._id.toString()});
+    }else{
+      res.status(401).json({
+        message: "Invalid Email or Password!!"
+      });
+    }
+
+  } catch (error) {
+    res.status(500).json("Internal Server Error!!");
+  }
+}
+
+module.exports = { home, register, login};
