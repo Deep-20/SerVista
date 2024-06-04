29cc0f1ff19c1de65d9a2d55110fbdae2b7912f7
Deep-20 <49397575+Deep-20@users.noreply.github.com>
2024-06-04 01:49:14 -0400

    Added Footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,22 +5,26 @@ import { Contact } from "./pages/Contact";
 import { Service } from "./pages/Service";
 import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
-import { Navbar } from ".//components/Navbar";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
 
 const App = () => {
-  return <>
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/contact" element={<Contact />}/>
-        <Route path="/service" element={<Service />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route path="/login" element={<Login />}/>
-      </Routes>
-    </BrowserRouter>
-  </>
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <p> @theTechElevators 2024 </p>
+      </div>
+    </>
+  );
+};
