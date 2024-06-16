e14b04d76f8cfa8cf9efe76a2f412175e8cf1a13
Deep-20 <49397575+Deep-20@users.noreply.github.com>
2024-06-16 00:14:34 -0400

    Added JWT Authorization in backend

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -9,6 +9,10 @@ const home = async (req, res) => {
   }
 };
 
+// * -------------------------
+// * Registration Logic
+// * -------------------------
+
 const register = async (req, res) => {
   try {
     const { username, email, phone, password } = req.body;
@@ -29,42 +33,60 @@ const register = async (req, res) => {
       password,
     });
 
-    res.status(201).json({ 
-      message: "Registration Successful", 
-      token: await UserCreated.generateToken(), 
-      userId: UserCreated._id.toString()
+    res.status(201).json({
+      message: "Registration Successful",
+      token: await UserCreated.generateToken(),
+      userId: UserCreated._id.toString(),
     });
   } catch (error) {
     res.status(500).json("Internal Server Error!!");
   }
 };
 
-const login = async(req, res) => {
-  try {
-    const {email, password} = req.body;
+// * -------------------------
+// * Login Logic
+// * -------------------------
 
-    const userExist = await User.findOne({email});
-    
-    if(!userExist){
-      return res.status(400).json({message: "Invalid Credential!!"});
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+
+    if (!userExist) {
+      return res.status(400).json({ message: "Invalid Credential!!" });
     }
 
     const user = await userExist.comparePassword(password);
 
-    if(user){
-      res.status(200).json({ 
-        message: "Login Successful", 
-        token: await userExist.generateToken(), 
-        userId: userExist._id.toString()});
-    }else{
+    if (user) {
+      res.status(200).json({
+        message: "Login Successful",
+        token: await userExist.generateToken(),
+        userId: userExist._id.toString(),
+      });
+    } else {
       res.status(401).json({
-        message: "Invalid Email or Password!!"
+        message: "Invalid Email or Password!!",
       });
     }
-
   } catch (error) {
     res.status(500).json("Internal Server Error!!");
   }
-}
+};
 
-module.exports = { home, register, login};
+// * -------------------------
+// * To send User data - User Logic
+// * -------------------------
+
+const user = async (req, res) => {
+  try {
+    const userData = req.user;
+    console.log(userData);
+    res.status(200).json({ msg: userData });
+  } catch (error) {
+    console.log(`error from user route ${error}`);
+  }
+};
+
+module.exports = { home, register, login, user };
--- server/route/auth-route.js
+++ server/route/auth-route.js
@@ -3,9 +3,15 @@ const router = express.Router();
 const authcontrollers = require("../controllers/auth-controller");
 const validator = require("../validators/auth-validators");
 const validate = require("../middlewares/validate-middleware");
+const authMiddleware = require("../middlewares/auth-middleware");
 
 router.route("/").get(authcontrollers.home);
-router.route("/register").post(validate(validator.signupSchema), authcontrollers.register);
-router.route("/login").post(validate(validator.loginSchema), authcontrollers.login);
+router
+  .route("/register")
+  .post(validate(validator.signupSchema), authcontrollers.register);
+router
+  .route("/login")
+  .post(validate(validator.loginSchema), authcontrollers.login);
+router.route("/user").get(authMiddleware, authcontrollers.user);
 
 module.exports = router;
