78ec145618d9d656ea59d4312f777a5e657cbe61
Deep-20 <49397575+Deep-20@users.noreply.github.com>
2024-06-16 00:59:28 -0400

    Fetching data and adding it to Contact form

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useAuth } from "../store/auth";
 
 export const Contact = () => {
   const [contact, setContact] = useState({
@@ -7,6 +8,19 @@ export const Contact = () => {
     message: "",
   });
 
+  const [userData, setUserData] = useState(true);
+
+  const { user } = useAuth();
+
+  if (userData && user) {
+    setContact({
+      username: user.username,
+      email: user.email,
+      message: "",
+    });
+
+    setUserData(false);
+  }
   const handleInput = (e) => {
     const name = e.target.name;
     const value = e.target.value;
--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -1,10 +1,11 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 // eslint-disable-next-line react/prop-types
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState("");
 
   const storeTokenInLS = (serverToken) => {
     return localStorage.setItem("token", serverToken);
@@ -17,8 +18,37 @@ export const AuthProvider = ({ children }) => {
     setToken("");
     localStorage.removeItem("token");
   };
+
+  //JWT Authentication - to get the currently loggedIn user data
+  const userAuthentication = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/user", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      console.log("Response ok: ", response.ok);
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("User Daata gotten", data.userData);
+        setUser(data.userData);
+      }
+    } catch (error) {
+      console.log("Error fetching user data");
+    }
+  };
+
+  useEffect(() => {
+    userAuthentication();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ isLoggedIn, storeTokenInLS, LogoutUser }}>
+    <AuthContext.Provider
+      value={{ isLoggedIn, storeTokenInLS, LogoutUser, user }}
+    >
       {children}
     </AuthContext.Provider>
   );
